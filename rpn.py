7fb01e12c02b741e2e9d39cecaf4d008c164113e
cbnguyen <cbnguyen@umich.edu>
2018-11-04 17:47:10 -0500

    changed to be very similar to the rpn.py from c4cs

--- rpn.py
+++ rpn.py
@@ -10,30 +10,29 @@ op = {
 
 def calculate(arg):
     #stack for calculator
-    stack = arg.split()
+    stack = list()
 
     #process tokens
-    while len(stack) > 1:
-        token = stack.pop()
+    for token in arg.split():
         try:
-            value = int(token)
-            stack.append(value)
+            token = int(token)
+            stack.append(token)
         except ValueError:
-            val2 = int(stack.pop())
-            val1 = int(stack.pop())
+            val2 = stack.pop()
+            val1 = stack.pop()
 
             #Look up function in table
             func = op[token]
             result = func(val1, val2)
 
-            stack.append(str(result))
+            stack.append(result)
 
-    return int(stack[0])
+    return stack.pop()
 
 def main():
     while True:
         result = calculate(input('rpn calc> '))
-        print(result)
+        print("Result: ", result)
 
 if __name__ == '__main__':
     main()
